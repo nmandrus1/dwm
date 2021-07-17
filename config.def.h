15a0e469a66ff2e1df86b81fa75578cc3e5ea3b5
Niels Mandrus <nmandrus1@gmail.com>
2021-07-17 18:24:09 -0400

    changed scratchpad to st bc alacritty wasnt working

--- config.def.h
+++ config.def.h
@@ -12,7 +12,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 0;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 7;        /* vertical padding for statusbar */
-static const char *fonts[]          = {"SourceCodePro-Regular:size=12", "FontAwesome:size=12"};
+static const char *fonts[]          = {"SourceCodePro-Regular:size=12", "FontAwesome:size=18"};
 static const char dmenufont[]       = "SourceCodePro-Regular:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -92,7 +92,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 static const char scratchpadname[] = "scratchpad";
-static const char *scratchpadcmd[] = { "alacritty", "", scratchpadname, "", "120x34", NULL };
+static const char *scratchpadcmd[] = { "st", "-t", scratchpadname, "-g", "120x34", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
